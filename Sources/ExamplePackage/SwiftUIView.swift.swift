4d872506f9c0086b97de42ce2548c4857c210b80
Dishant Nagpal <dishantnagpal10@gmail.com>
2023-04-29 17:29:11 +0530

    Package added

---
+++ Sources/ExamplePackage/SwiftUIView.swift.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  ExamplePackage
+//
+//  Created by Dishant Nagpal on 29/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct SwiftUI:View{
+    
+    public var textString : String = ""
+    public var body: some View{
+        
+        Text("This is a swiftUI view!")
+    }
+    
+    public init(textString : String){
+        self.textString = textString
+    }
+    
+}
+ 
